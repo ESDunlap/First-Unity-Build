480c349301f4e3c16527106162ee414433dea88f
ESDunlap <dunlape3@my.erau.edu>
2025-01-15 13:51:04 -0700

    Updating to Web Publishment

---
+++ First Unity Test/Assets/Player.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * 0.1f;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * 0.1f;
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.position += Vector3.up * 0.1f;
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.position += Vector3.down * 0.1f;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * 0.1f;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * 0.1f;
+        }
+    }
+}
